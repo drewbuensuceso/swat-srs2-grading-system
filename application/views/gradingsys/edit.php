5ce22069932fecaf6735cac3530b7193924debfe
drewbuensuceso <42257616+drewbuensuceso@users.noreply.github.com>
2018-08-22 23:37:37 +0800

    Add files via upload
    added minimal validation for grade values.

--- application/views/gradingsys/edit.php
+++ application/views/gradingsys/edit.php
@@ -24,31 +24,31 @@
 		<div class = "form-group">
 		<label for="Math" class="col-md-2 text-right">Math</label>
 		<div class = "col-md-10">
-			<input type="number" value = "<?php echo $students->Math;?>" name="Math">
+			<input type="number" min="0" max="100" value = "<?php echo $students->Math;?>" name="Math">
 		</div>
 	</div>
 	<div class = "form-group">
 		<label for="Science" class="col-md-2 text-right">Science</label>
 		<div class = "col-md-10">
-			<input type="number" value = "<?php echo $students->Science?>" name="Science">
+			<input type="number" min="0" max="100" value = "<?php echo $students->Science?>" name="Science">
 		</div>
 	</div>
 	<div class = "form-group">
 		<label for="English" class="col-md-2 text-right">English</label>
 		<div class = "col-md-10">
-			<input type="number" value = "<?php echo $students->English;?>" name="English">
+			<input type="number" min="0" max="100" value = "<?php echo $students->English;?>" name="English">
 		</div>
 	</div>
 	<div class = "form-group">
 		<label for="Filipino" class="col-md-2 text-right">Filipino</label>
 		<div class = "col-md-10">
-			<input type="number" value = "<?php echo $students->Filipino;?>" name="Filipino">
+			<input type="number" min="0" max="100" value = "<?php echo $students->Filipino;?>" name="Filipino">
 		</div>
 	</div>
 	<div class = "form-group">
 		<label for="Mapeh" class="col-md-2 text-right">MAPEH</label>
 		<div class = "col-md-10">
-			<input type="number" value = "<?php echo $students->Mapeh;?>"name="Mapeh">
+			<input type="number" min="0" max="100" value = "<?php echo $students->Mapeh;?>"name="Mapeh">
 		</div>
 	</div>	
 	<div class = "form-group">
